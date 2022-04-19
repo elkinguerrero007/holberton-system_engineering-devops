c43d819919183a9c45b58a5ac8de50d3e23b81a9
elkinguerrero007 <3439@holbertonschool.com>
2022-04-19 22:44:00 +0000

    ssh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# install package with puppet
-# Install a package
-package { 'flask' :
-     ensure        => 'present',
-     pkgname       => 'requests',
-     pip_provider  => 'pip3',
-     virtualenv    => '/var/www/project1',
-     owner         => 'root',
-     timeout       => 1800
-   }
+# Using Puppet, install puppet-lint.
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3'
+}
