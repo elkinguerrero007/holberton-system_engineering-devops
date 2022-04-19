64bb859d6c35d549a3a3234b597a40e52e3fbaab
elkinguerrero007 <3439@holbertonschool.com>
2022-04-19 20:27:28 +0000

    create_file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# File creation with puppet love
+file {'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
